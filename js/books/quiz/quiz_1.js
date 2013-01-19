ee4a47ffc022136e64123a0dfe9be923795acdb3
unknown <a@Comp-2.(none)>
2013-01-19 14:39:12 +0530

    up

--- js/books/quiz/quiz_1.js
+++ js/books/quiz/quiz_1.js
@@ -36,9 +36,28 @@ function createFirstQuestion()
 	qEngine.setScore(false);
 }
 
-function playSound(soundfile) {
+/*function playSound(soundfile) {
 	var audio = document.getElementById(soundfile.id);
 	audio.play();
+}*/
+
+function playSound(src) {
+	if (device.platform == 'Android') {
+		src = '/android_asset/www/' + src;
+	}
+
+	var media = new Media(src, success, error_error);
+
+	media.play();
+}
+
+function success() {
+	// ignore
+}
+
+function error_error(e) {
+	alert('great error');
+	alert(e.message);
 }
 
 
